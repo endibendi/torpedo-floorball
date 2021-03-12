3bb95acb1d9b5db03769d43730761aab5494c093
Endre <endibend@gmail.com>
2021-03-12 12:44:10 +0100

    added scss

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby SE Starter`,
+    description: `Starter for my gatsby projects`,
+    author: `@sewbsite`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -16,6 +16,7 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-sass`,
 
     // ----------- For PWA --------------
     // {
