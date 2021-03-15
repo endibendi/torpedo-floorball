f26fdf68f05f92b0d31e3a33b4fb5e8915ceeb77
Endre <endibend@gmail.com>
2021-03-15 15:16:21 +0100

    footer layout

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,9 +18,10 @@ module.exports = {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
         fonts: [
-          `roboto`,
+          `roboto\:500,700`,
           `Raleway\:800`,
-          `source sans pro\:300,400,400i,700`, // you can also specify font weights and styles
+          // `source sans pro\:300,400,400i,700`,
+          // you can also specify font weights and styles
         ],
         display: "swap",
       },
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,30 @@
 import React from "react"
 import * as styles from "./footer.module.scss"
+import { FaFacebookSquare } from "react-icons/fa"
 
 const Footer = () => {
   return (
     <footer>
       <div className="container">
+        <div className={styles.footerInfo}>
+          <div className="footerContent">
+            <h4>Edzések</h4>
+            <div className={styles.footerInfoContainer}>
+              <h5>Felnőtt edzésidőpontok:</h5>
+              <p>Hétfő: 19:00 - 20:30</p>
+              <p>Csütörtök: 19:00-20:30</p>
+              <h5 className={styles.pt8}>Helyszín:</h5>
+              <p>ATTE Sportcsarnok</p>
+              <p>1139 Budapest, Rozsnyai u. 4.</p>
+            </div>
+          </div>
+          <div className={styles.footerSocial}>
+            <FaFacebookSquare />
+          </div>
+        </div>
         <div className={styles.center}>
-          <span>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.com">Gatsby</a>
+          <span className={styles.copy}>
+            © {new Date().getFullYear()} Torpedo Floorball
           </span>
         </div>
       </div>
