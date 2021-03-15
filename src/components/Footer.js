6771c113aeb853d194805304f7069294bf686ac9
Endre <endibend@gmail.com>
2021-03-15 14:45:48 +0100

    footer module scss

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
 import React from "react"
+import * as styles from "./footer.module.scss"
 
 const Footer = () => {
   return (
     <footer>
       <div className="container">
-        <div className="center">
+        <div className={styles.center}>
           <span>
             © {new Date().getFullYear()}, Built with
             {` `}
