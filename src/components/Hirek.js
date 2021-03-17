ae648078115d4f1f6770a81f81083ea70d21d4ce
Endre <endibend@gmail.com>
2021-03-17 08:04:25 +0100

    hirek grid layout done

--- src/components/Hirek.js
+++ src/components/Hirek.js
@@ -7,84 +7,101 @@ import * as styles from "./hirek.module.scss"
 
 const Hirek = () => {
   return (
-    <>
+    <section id="hirek">
       <div className={styles.hirekWrapper}>
         <h2>Hírek</h2>
-        <article className="articleCard">
-          <StaticImage
-            src="../images/defaulthir.jpg"
-            alt="article image alt"
-            className={styles.articleImg}
-            objectFit="cover"
-            objectPosition="50% 20%"
-          />
-          <Link to="" className={styles.infoLink}>
-            <div className={styles.info}>
-              <h3>Kazinbarcikai Ördögök B - Torpedo ATTE 3:10</h3>
-              <span>2021.03.16</span>
-            </div>
-          </Link>
-        </article>
-        <article className="articleCard">
-          <StaticImage
-            src="../images/defaulthir.jpg"
-            alt="article image alt"
-            className={styles.articleImg}
-            objectFit="cover"
-            objectPosition="50% 20%"
-          />
-          <Link to="" className={styles.infoLink}>
-            <div className={styles.info}>
-              <h3>Kazinbarcikai Ördögök B - Torpedo ATTE 3:10</h3>
-              <span>2021.03.16</span>
-            </div>
-          </Link>
-        </article>
-        <article className="articleCard">
-          <StaticImage
-            src="../images/defaulthir.jpg"
-            alt="article image alt"
-            className={styles.articleImg}
-            objectFit="cover"
-            objectPosition="50% 20%"
-          />
-          <Link to="" className={styles.infoLink}>
-            <div className={styles.info}>
-              <h3>Kazinbarcikai Ördögök B - Torpedo ATTE 3:10</h3>
-              <span>2021.03.16</span>
-            </div>
-          </Link>
-        </article>
-        <article className="articleCard">
-          <StaticImage
-            src="../images/defaulthir.jpg"
-            alt="article image alt"
-            className={styles.articleImg}
-            objectFit="cover"
-            objectPosition="50% 20%"
-          />
-          <Link to="" className={styles.infoLink}>
-            <div className={styles.info}>
-              <h3>Kazinbarcikai Ördögök B - Torpedo ATTE 3:10</h3>
-              <span>2021.03.16</span>
-            </div>
-          </Link>
-        </article>
-        <article className="articleCard">
-          <StaticImage
-            src="../images/defaulthir.jpg"
-            alt="article image alt"
-            className={styles.articleImg}
-            objectFit="cover"
-            objectPosition="50% 20%"
-          />
-          <Link to="" className={styles.infoLink}>
-            <div className={styles.info}>
-              <h3>Kazinbarcikai Ördögök B - Torpedo ATTE 3:10</h3>
-              <span>2021.03.16</span>
-            </div>
-          </Link>
-        </article>
+        <div className={styles.articleWrapper}>
+          <article className="articleCard">
+            <StaticImage
+              src="../images/defaulthir.jpg"
+              alt="article image alt"
+              className={styles.articleImg}
+              objectFit="cover"
+              objectPosition="50% 20%"
+            />
+            <Link to="" className={styles.infoLink}>
+              <div className={styles.info}>
+                <h3>Kazinbarcikai Ördögök B - Torpedo ATTE 3:10</h3>
+                <span>2021.03.16</span>
+              </div>
+            </Link>
+          </article>
+          <article className="articleCard">
+            <StaticImage
+              src="../images/defaulthir.jpg"
+              alt="article image alt"
+              className={styles.articleImg}
+              objectFit="cover"
+              objectPosition="50% 20%"
+            />
+            <Link to="" className={styles.infoLink}>
+              <div className={styles.info}>
+                <h3>Kazinbarcikai Ördögök B - Torpedo ATTE 3:10</h3>
+                <span>2021.03.16</span>
+              </div>
+            </Link>
+          </article>
+          <article className="articleCard">
+            <StaticImage
+              src="../images/defaulthir.jpg"
+              alt="article image alt"
+              className={styles.articleImg}
+              objectFit="cover"
+              objectPosition="50% 20%"
+            />
+            <Link to="" className={styles.infoLink}>
+              <div className={styles.info}>
+                <h3>Kazinbarcikai Ördögök B - Torpedo ATTE 3:10</h3>
+                <span>2021.03.16</span>
+              </div>
+            </Link>
+          </article>
+          <article className="articleCard">
+            <StaticImage
+              src="../images/defaulthir.jpg"
+              alt="article image alt"
+              className={styles.articleImg}
+              objectFit="cover"
+              objectPosition="50% 20%"
+            />
+            <Link to="" className={styles.infoLink}>
+              <div className={styles.info}>
+                <h3>Kazinbarcikai Ördögök B - Torpedo ATTE 3:10</h3>
+                <span>2021.03.16</span>
+              </div>
+            </Link>
+          </article>
+          <article className="articleCard">
+            <StaticImage
+              src="../images/defaulthir.jpg"
+              alt="article image alt"
+              className={styles.articleImg}
+              objectFit="cover"
+              objectPosition="50% 20%"
+            />
+            <Link to="" className={styles.infoLink}>
+              <div className={styles.info}>
+                <h3>Kazinbarcikai Ördögök B - Torpedo ATTE 3:10</h3>
+                <span>2021.03.16</span>
+              </div>
+            </Link>
+          </article>
+          <article className="articleCard">
+            <StaticImage
+              src="../images/defaulthir.jpg"
+              alt="article image alt"
+              className={styles.articleImg}
+              objectFit="cover"
+              objectPosition="50% 20%"
+            />
+            <Link to="" className={styles.infoLink}>
+              <div className={styles.info}>
+                <h3>Kazinbarcikai Ördögök B - Torpedo ATTE 3:10</h3>
+                <span>2021.03.16</span>
+              </div>
+            </Link>
+          </article>
+        </div>
       </div>
       <div className={styles.slider}>
         <FaAngleLeft className={styles.sliderIcon} />
@@ -95,7 +112,7 @@ const Hirek = () => {
         </div>
         <FaAngleRight className={styles.sliderIcon} />
       </div>
-    </>
+    </section>
   )
 }
 
