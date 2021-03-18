c9bfc10be7d04ceb83475eb104cfe5efa80e1a96
Endre <endibend@gmail.com>
2021-03-18 16:48:25 +0100

    navbar

--- src/components/IndexLayout.js
+++ src/components/IndexLayout.js
@@ -17,7 +17,7 @@ const Layout = ({ children }) => {
   return (
     <>
       <div className="flex-container">
-        <Navbar toggle={toggle} className="index-nav" />
+        <Navbar toggle={toggle} />
         <Sidebar isOpen={isOpen} toggle={toggle} />
         <main>{children}</main>
         <Footer />
