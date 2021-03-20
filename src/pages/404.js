946ed9af4e0b5c6f963bea35cf51cdcde7bcc35d
Endre <endibend@gmail.com>
2021-03-20 09:01:25 +0100

    error page done, h1 h2 color changed to primary

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,19 @@
 import * as React from "react"
+import { Link } from "gatsby"
 
 import Layout from "../components/Layout"
 import SEO from "../components/_seo"
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="404" />
+    <div className="container">
+      <h1>404: Not Found</h1>
+      <p>A keresett oldal nem található!</p>
+      <Link to="/" style={{ paddingTop: "20px" }}>
+        Vissza a főoldalra
+      </Link>
+    </div>
   </Layout>
 )
 
