209a90fb32dc1a6d47359a668466a8bcd629b725
Endre <endibend@gmail.com>
2021-05-16 08:35:24 +0200

    csapat tagok darabolva pozicio szserint

--- src/components/CsapatList.js
+++ src/components/CsapatList.js
@@ -9,23 +9,70 @@ const CsapatList = ({ tagok = [] }) => {
       <div className={styles.articleWrapper}>
         {tagok.map((tag, id) => {
           const { nev, szerep, tagkep } = tag
-
-          return (
-            <article key={id}>
-              <GatsbyImage
-                image={tagkep.gatsbyImageData}
-                className={styles.articleImg}
-                alt="tag kep"
-                objectFit="cover"
-                objectPosition="center top"
-                quality="100"
-              />
-              <div className={styles.articleInfo}>
-                <h3>{nev}</h3>
-                <p>{szerep}</p>
-              </div>
-            </article>
-          )
+          if (szerep === "védő") {
+            return (
+              <article key={id}>
+                <GatsbyImage
+                  image={tagkep.gatsbyImageData}
+                  className={styles.articleImg}
+                  alt="tag kep"
+                  objectFit="cover"
+                  objectPosition="center top"
+                  quality="100"
+                />
+                <div className={styles.articleInfo}>
+                  <h3>{nev}</h3>
+                  <p>{szerep}</p>
+                </div>
+              </article>
+            )
+          }
+        })}
+      </div>
+      <div className={styles.articleWrapper}>
+        {tagok.map((tag, id) => {
+          const { nev, szerep, tagkep } = tag
+          if (szerep === "támadó") {
+            return (
+              <article key={id}>
+                <GatsbyImage
+                  image={tagkep.gatsbyImageData}
+                  className={styles.articleImg}
+                  alt="tag kep"
+                  objectFit="cover"
+                  objectPosition="center top"
+                  quality="100"
+                />
+                <div className={styles.articleInfo}>
+                  <h3>{nev}</h3>
+                  <p>{szerep}</p>
+                </div>
+              </article>
+            )
+          }
+        })}
+      </div>
+      <div className={styles.articleWrapper}>
+        {tagok.map((tag, id) => {
+          const { nev, szerep, tagkep } = tag
+          if (szerep === "kapus") {
+            return (
+              <article key={id}>
+                <GatsbyImage
+                  image={tagkep.gatsbyImageData}
+                  className={styles.articleImg}
+                  alt="tag kep"
+                  objectFit="cover"
+                  objectPosition="center top"
+                  quality="100"
+                />
+                <div className={styles.articleInfo}>
+                  <h3>{nev}</h3>
+                  <p>{szerep}</p>
+                </div>
+              </article>
+            )
+          }
         })}
       </div>
     </div>
