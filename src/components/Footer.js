f823060e16d5b67152d283c67c12150e10b1502f
Endre <endibend@gmail.com>
2021-08-31 09:18:46 +0200

    events added to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,7 @@ import React from "react"
 import * as styles from "./footer.module.scss"
 import { StaticImage } from "gatsby-plugin-image"
 import { FaFacebookSquare, FaInstagramSquare } from "react-icons/fa"
+import Events from "./Events"
 
 const Footer = () => {
   return (
@@ -9,15 +10,19 @@ const Footer = () => {
       <div className="container">
         <div className={styles.footerWrapper}>
           <div className={styles.footerInfo}>
-            <div className="footerContent">
-              <h4>Edzések</h4>
+            <div className={styles.footerContent}>
               <div className={styles.footerInfoContainer}>
+                <h4>Edzések</h4>
                 <h5>Felnőtt edzésidőpontok:</h5>
                 <p>Hétfő: 19:00 - 20:30</p>
                 <p>Csütörtök: 19:00-20:30</p>
                 <h5 className={styles.pt8}>Helyszín:</h5>
                 <p>ATTE Sportcsarnok</p>
                 <p>1139 Budapest, Rozsnyai u. 4.</p>
+              </div>
+              <div className={styles.footerInfoContainer}>
+                <h4>Közelgő események:</h4>
+                <Events />
               </div>
             </div>
             <div className={styles.footerSocial}>
